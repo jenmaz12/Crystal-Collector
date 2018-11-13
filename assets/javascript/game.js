b66a8e0404cf9b85947723c30933db78e834ab62
jenmaz12 <jenmaz12@gmail.com>
2018-11-13 06:06:06 -0800

    trying to assign crystal values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,4 +10,16 @@ var userTotal = 0;
 
 // create array of crystal values
 var numberOptions = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+// create variable for crystal images
+var imageCrystal = $("<img>");
+
+// assign value randomly from array above
+imageCrystal.attr("data-crystalvalue",numberOptions[Math.floor(Math.random() * 11)]);
+
+$(".crystal").on("click",function(){
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    console.log(crystalValue);
+})
 });
