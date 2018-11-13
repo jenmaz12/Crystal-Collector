80a737a676fd91b9f2d88854877d96b287a06066
jenmaz12 <jenmaz12@gmail.com>
2018-11-12 18:10:47 -0800

    working target number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,7 @@
-var targetNumber = 0;
-targetNumber = Math.floor(Math.random() * 120) + 19;
-$("#targetnumber").text(targetNumber);
+$(document).ready(function(){
+// set targetNumber equal to a random integer between 120 and 19
+var targetNumber = Math.floor(Math.random() * 120) + 19;
+// print target number to section with id targetnumber
+$("#targetnumber").html(targetNumber);
 console.log(targetNumber);
+});
