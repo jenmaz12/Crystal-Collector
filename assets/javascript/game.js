da5d430af5b06b1c99dbcf64f25efbb328e0fefa
jenmaz12 <jenmaz12@gmail.com>
2018-11-13 05:26:53 -0800

    setting userTotal to zero and creating array of numberOptions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,4 +4,10 @@ var targetNumber = Math.floor(Math.random() * 120) + 19;
 // print target number to section with id targetnumber
 $("#targetnumber").html(targetNumber);
 console.log(targetNumber);
+
+// set usertotal to 0
+var userTotal = 0;
+
+// create array of crystal values
+var numberOptions = [1,2,3,4,5,6,7,8,9,10,11,12];
 });
