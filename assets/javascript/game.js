b70c21d1f0daba79372781ba00b0aadeda176dad
jenmaz12 <jenmaz12@gmail.com>
2018-11-15 20:08:06 -0800

    working crystal value

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,11 +11,18 @@ var userTotal = 0;
 // create array of crystal values
 var numberOptions = [1,2,3,4,5,6,7,8,9,10,11,12];
 
-// create variable for crystal images
-var imageCrystal = $("<img>");
+// create variable for each crystal based on their id
+var crystal1 = $("#crystal1");
+var crystal2 = $("#crystal2");
+var crystal3 = $("#crystal3");
+var crystal4 = $("#crystal4");
 
-// assign value randomly from array above
-imageCrystal.attr("data-crystalvalue",numberOptions[Math.floor(Math.random() * 11)]).toString();
+// assign value randomly to each from array above
+crystal1.attr("data-crystalvalue",numberOptions[Math.floor(Math.random() * 11)]);
+crystal2.attr("data-crystalvalue",numberOptions[Math.floor(Math.random() * 11)]);
+crystal3.attr("data-crystalvalue",numberOptions[Math.floor(Math.random() * 11)]);
+crystal4.attr("data-crystalvalue",numberOptions[Math.floor(Math.random() * 11)]);
+
 
 $(".crystal").on("click",function(){
     var crystalValue = ($(this).attr("data-crystalvalue"));
